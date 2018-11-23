c3c3b5b47d008e491240586d0d49624fdb702f54
DMendez49 <31753767+DMendez49@users.noreply.github.com>
2018-11-23 15:30:19 -0700

    made overloaded contructor for io class

--- src/drop7Clone/inputOutput.java
+++ src/drop7Clone/inputOutput.java
@@ -12,11 +12,21 @@ public class inputOutput
 		private Color[] colors = new Color[16];
 		private File colorFile;
 		private File ScoreFile;
+		public inputOutput() throws FileNotFoundException
+		{
+		colorFile = new File("/Users/davidmendez/code/Eclipse /workspace/Drop7Clone/src/drop7Clone");
+		Scanner colorSc = new Scanner(colorFile);
+		for(int x = 0; x<16; x++)
+			{
+				colorCodes[x] = colorSc.nextLine();
+				colors[x] = Color.decode(colorCodes[x]);
+			}
+		}
 		public inputOutput(File f) throws FileNotFoundException
 		{
 	
 			
-			File q = new File(colors.txt);
+		
 			colorFile = f;
 			Scanner colorSc = new Scanner(colorFile);
 			for(int x = 0; x<16; x++)
