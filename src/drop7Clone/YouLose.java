7c6b82d1bfacb90f20988b84bdff11d537e7f1d8
DMendez49 <31753767+DMendez49@users.noreply.github.com>
2018-12-02 21:18:26 -0700

    you lose update added j labels high score

--- src/drop7Clone/YouLose.java
+++ src/drop7Clone/YouLose.java
@@ -58,39 +58,31 @@ public class YouLose extends JFrame {
 		
 		JButton btnNewButton_1 = new JButton("Exit Game");
 		btnNewButton_1.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				
-				welcomeGui.DISPOSE_ON_CLOSE();
-				//YouLose pho = new YouLose(vio);
-				//pho.setVisible(true);
-				
-				
+			public void actionPerformed(ActionEvent e) {	
+				System.exit(0);
 			}
 		});
 		panel.add(btnNewButton_1);
 		
 		JButton btnNewButton = new JButton("New Game");
 		btnNewButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				
-				
-				
-				
+			public void actionPerformed(ActionEvent e) {	
+				welcomeGui.DISPOSE_ON_CLOSE();
+				welcomeGui pho = new welcomeGui();
+				pho.setVisible(true);	
 			}
 		});
 		panel.add(btnNewButton);
 		
-		JLabel highScore1 = new JLabel("Top High Score: David Mendez:0550123");
-		highScore1.setOpaque(true);
-		highScore1.setBackground(Color.DARK_GRAY);
-		highScore1.setHorizontalAlignment(SwingConstants.CENTER);
-		highScore1.setForeground(new Color(255, 250, 205));
-		highScore1.setFont(new Font("Monaco", Font.BOLD, 10));
-		//.add(highScore1);
+		
 		
 		JPanel panel_1 = new JPanel();
 		panel.add(panel_1);
 		panel_1.setLayout(new GridLayout(0, 2, 0, 0));
+	}
+	protected static void EXIT_ON_CLOSE() {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
